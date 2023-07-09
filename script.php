b1eb1e540442148b0f341565eff4da2a0ccfc3af
anlide <elfanlide@gmail.com>
2023-07-09 22:44:40 +0200

    Make work plan

---
+++ script.php
@@ -0,0 +1,7 @@
+<?php
+// TODO: create empty directories (delete previous run)
+// TODO: unzip file "work.zip" to temporary folder
+// TODO: put extracted files from the temporary folder to main folder
+// TODO: parse ".xhtml" file
+// TODO: build JSON from the parsed file
+// TODO: report output
