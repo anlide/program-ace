02d131dcad4bcc5b11e37ae6d6a72033edc2c70a
anlide <elfanlide@gmail.com>
2023-07-09 22:50:51 +0200

    Recreate "work" and "tmp" folders

--- script.php
+++ script.php
@@ -1,6 +1,25 @@
 <?php
-// TODO: create empty directories (delete previous run)
-// TODO: unzip file "work.zip" to temporary folder
+function rrmdir($dir) {
+  if (is_dir($dir)) {
+    $objects = scandir($dir);
+    foreach ($objects as $object) {
+      if ($object != "." && $object != "..") {
+        if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
+          rrmdir($dir. DIRECTORY_SEPARATOR .$object);
+        else
+          unlink($dir. DIRECTORY_SEPARATOR .$object);
+      }
+    }
+    rmdir($dir);
+  }
+}
+
+// NOTE: create empty directories (delete previous run)
+rrmdir('work');
+mkdir('work');
+rrmdir('tmp');
+mkdir('tmp');
+// TODO: unzip file "test.zip" to temporary folder
 // TODO: put extracted files from the temporary folder to main folder
 // TODO: parse ".xhtml" file
 // TODO: build JSON from the parsed file
