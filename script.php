76adb8e284cebf7ea368507503b512da1a039613
anlide <elfanlide@gmail.com>
2023-07-10 00:35:55 +0200

    Implement copy tmp -> work files

--- script.php
+++ script.php
@@ -1,4 +1,10 @@
 <?php
+const PATH_TMP = 'tmp';
+const PATH_WORK = 'work';
+const PATH_WORK_XHTML = 'work/name_project';
+const PATH_WORK_IMG = 'work/images';
+const PATH_WORK_CSS = 'work/styleImages';
+
 /**
  * @param $dir
  * @return void
@@ -35,15 +41,25 @@ function extractZipToTmp(): void {
 /**
  * find all ".xhtml" files, throw an Exception if there are few files
  * @param string $path
- * @param ?string $filename
  * @return string
  * @throws Exception
  */
-function findXhtmlFile(string $path, ?string &$filename): string {
-  $filename = 'filename.xhtml';
-  throw new Exception('No any xhtml file exists at provided path');
+function findXhtmlFile(string $path): string {
+  $filename = null;
+  $dir = new RecursiveDirectoryIterator($path);
+  foreach (new RecursiveIteratorIterator($dir) as $fullName => $file) {
+    if (str_contains($file->getFilename(), 'xhtml')) {
+      if ($filename !== null) {
+        throw new Exception('There are few xhtml in the zip');
+      }
+      $filename = $fullName;
+    }
+  }
+  if ($filename === null) {
+    throw new Exception('No any xhtml file exists at provided path');
+  }
 
-  return '';
+  return $filename;
 }
 
 /**
@@ -53,8 +69,23 @@ function findXhtmlFile(string $path, ?string &$filename): string {
  * @throws Exception
  */
 function recursiveCopy($from, $to): void {
-  if ($from) {
+  if (!is_dir($from)) {
+    throw new Exception('Source not exists');
+  }
+  if (!is_dir($to)) {
     throw new Exception('Destination not exists');
+  }
+
+  foreach (
+    $iterator = new \RecursiveIteratorIterator(
+      new \RecursiveDirectoryIterator($from, \RecursiveDirectoryIterator::SKIP_DOTS),
+      \RecursiveIteratorIterator::SELF_FIRST) as $item
+  ) {
+    if ($item->isDir()) {
+      mkdir($to . '/' . $iterator->getSubPathname());
+    } else {
+      copy($item, $to . '/' . $iterator->getSubPathname());
+    }
   }
 }
 
@@ -63,20 +94,23 @@ function recursiveCopy($from, $to): void {
  * @throws Exception
  */
 function findAndCopyXhtmlToWork(): void {
-  $path = findXhtmlFile('tmp', $filename);
-  // TODO: copy file $filename
-  recursiveCopy($path.'/css', 'work/styleImages');
-  recursiveCopy($path.'/images', 'work/images');
+  $filename = findXhtmlFile(PATH_TMP);
+  if (!copy($filename, PATH_WORK_XHTML . '/' . pathinfo($filename)['basename'])) {
+    throw new Exception('Unable to copy file: ' . $filename);
+  }
+  $path = pathinfo($filename)['dirname'];
+  recursiveCopy($path . '/images', PATH_WORK_IMG);
+  recursiveCopy($path . '/css', PATH_WORK_CSS);
 }
 
 // NOTE: create empty directories (delete previous run)
-rrmdir('work');
-mkdir('work');
-mkdir('work/name_project');
-mkdir('work/images');
-mkdir('work/styleImages');
-rrmdir('tmp');
-mkdir('tmp');
+rrmdir(PATH_WORK);
+mkdir(PATH_WORK);
+mkdir(PATH_WORK_XHTML);
+mkdir(PATH_WORK_IMG);
+mkdir(PATH_WORK_CSS);
+rrmdir(PATH_TMP);
+mkdir(PATH_TMP);
 // NOTE: unzip file "test.zip" to temporary folder
 try {
   extractZipToTmp();
